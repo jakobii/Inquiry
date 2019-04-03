90f25c8a1952097a1f4d2bebc2e0c9ce2a219380
jakobii <jacob.ochoa@gmail.com>
2019-04-03 13:32:07 -0700

    pretty for merge to master

--- tests/test.sql
+++
@@ -1,2 +0,0 @@
-
-select top 1 * from [Employees]
--- tests/test5.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module "$psscriptroot\..\Inquiry.psm1" -Verbose
-
-$db = [DatabaseConnection]::New('localhost','MHUSD')
-$db.Debug = $true
-$tb = $db.Table('dbo','Employees')
-$row = $tb.Where(@{EmployeeID = 842975})
-$row.BargainingUnit = 33
